647a98800d24fa8f78211ee2d020b99d73593fe7
Pandiyarajan.P <95677822+Pandiyara@users.noreply.github.com>
2022-05-14 10:49:39 +0530

    Update factor.java

--- factor.java
+++ factor.java
@@ -1,24 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
-package my.mavenproject4;
-
-
 import java.util.Scanner;
 /**
  *
  * @author pandi
  */
 
-public class Mavenproject4 {
+public class factor {
 
      public static void main(String[] args) {
         
-           Scanner hack =new Scanner(System.in);
+           Scanner hack =new Scanner(System.in);  //hack is scanner variable name
            System.out.println("Enter the number: ");
-           int n=hack.nextInt();
+           int n=hack.nextInt();  //n is input variable 
              for(int i=1;i<=n;i++)
                {
                     if (n%i==0) {
